34749082ec85fa6a8db69722d9b59153f49019e1
SANIA ROSE <saniarose011@gmail.com>
2026-09-13 04:05:56 +0530

    Add mongoose import to Api.js

--- server/Routes/Api.js
+++ server/Routes/Api.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from  'express';
 import fetch from "node-fetch";
 import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 import path from "path";
 import {GoogleGenAI} from "@google/genai"
 import User from  '../../models/Users.js';
