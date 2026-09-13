44b85d9c8675733f65ba0d8d0ed65bccf776b9df
Sania <saniarose011@gmail.com>
2026-09-13 16:21:03 +0530

    Allow Render frontend host

--- Freelance/Freelance/vite.config.js
+++ Freelance/Freelance/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['mini-fiverr-01-5052.onrender.com']
+  }
 })
